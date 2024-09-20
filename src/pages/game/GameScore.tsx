f6234d3409678cac12e3f2f03d4493a99e6d0aab
dougflip <douglasdifilippo@gmail.com>
2024-09-20 11:28:05 -0400

    Use RemoteData for GameScore

--- src/pages/game/GameScore.tsx
+++ src/pages/game/GameScore.tsx
@@ -10,19 +10,12 @@ import { updateGame, useGame } from "@/db/db";
 
 import { GameScoreForm } from "@/components/game/GameScoreForm";
 import { GameScoreTable } from "@/components/game/GameScoreTable";
+import { RemoteData } from "@/components/remote-data/RemoteData";
 import { useParams } from "@tanstack/react-router";
 
 export function GameScore() {
   const { gameId } = useParams({ from: "/games/$gameId" });
-  const { data, error, isLoading } = useGame(gameId);
-
-  if (isLoading || !data) {
-    return "loading...";
-  }
-
-  if (error) {
-    return "error!";
-  }
+  const games = useGame(gameId);
 
   function handleScore({ game, score, turn }: PlayerScoreUpdate) {
     updateGame(setPlayerScore(game, score, turn));
@@ -32,24 +25,29 @@ export function GameScore() {
     updateGame(setTurn(game, turn));
   }
 
-  const game = data.games[0];
-
   return (
-    <div>
-      <GameScoreForm
-        key={`${game.turn.playerIndex}-${game.turn.roundIndex}`}
-        game={game}
-        playerIndex={game.turn.playerIndex}
-        roundIndex={game.turn.roundIndex}
-        initialScore={getScoreForTurn(game)}
-        onScore={handleScore}
-      />
-      <GameScoreTable
-        game={game}
-        playerIndex={game.turn.playerIndex}
-        roundIndex={game.turn.roundIndex}
-        onTurnClick={handleTurnClick}
-      />
-    </div>
+    <RemoteData
+      data={games}
+      render={({ games: [game] }) => {
+        return (
+          <div>
+            <GameScoreForm
+              key={`${game.turn.playerIndex}-${game.turn.roundIndex}`}
+              game={game}
+              playerIndex={game.turn.playerIndex}
+              roundIndex={game.turn.roundIndex}
+              initialScore={getScoreForTurn(game)}
+              onScore={handleScore}
+            />
+            <GameScoreTable
+              game={game}
+              playerIndex={game.turn.playerIndex}
+              roundIndex={game.turn.roundIndex}
+              onTurnClick={handleTurnClick}
+            />
+          </div>
+        );
+      }}
+    />
   );
 }
